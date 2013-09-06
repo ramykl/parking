cd10c5e87057d877f314c32e8e4b1c287b23738b
ramykl <r.loveridge@hotmail.com>
2013-09-06 20:05:38 +1000

    working circle detection

--- saveVideo.py
+++ saveVideo.py
@@ -1,33 +1,60 @@
-from freenect import sync_get_depth as get_depth, sync_get_video as get_video
 import cv2  
 import numpy as np
 import glob
 import sys
+import subprocess
+
+# x = 1280
+# y = 720
 
 
 def doloop():
-    global depth, rgb
-    path = "./kpictures/"
+    path = "./videos/"
     num = glob.glob(str(path) + "*.avi")
     i = len(num)
-    video = cv2.VideoWriter(path+'video'+str(i)+'.avi',cv2.cv.CV_FOURCC('D','I','V','X'),20,(1280,480))
+    video = cv2.VideoWriter(path+'video'+str(i)+'.avi',cv2.cv.CV_FOURCC('D','I','V','X'),20,(640,480))
     if not video.isOpened():
         print 'error with video opening'
         sys.exit(1)
+    
+    p1 = subprocess.Popen(['ls', '-l', '/dev/v4l/by-id'], stdout=subprocess.PIPE) #, '|', 'grep', 'HD-3000']
+    p2 = subprocess.Popen(['grep', 'HD-3000'], stdin = p1.stdout, stdout=subprocess.PIPE)
+    p1.stdout.close()
+    out = p2.communicate()[0]
+    if out == '':
+        print 'webcam not attached'
+        sys.exit(1)
+    cam = int(out[-2])
+    
+    """
+    to get video number for specific camera look in /dev/v4l/by-id "ls -l" 
+    """
+    stream = cv2.VideoCapture(cam)    
+    if not stream.isOpened():
+        print 'error opening stream'
+        sys.exit(1)
+    
+    subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Exposure, Auto', "3"])
+    subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Contrast', "1"])
+    subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Brightness', "150"])
+    subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Saturation', "75"])
+    subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Sharpness', "25"])
+    
+#     stream.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, y)
+#     stream.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, x)
+
     print 'press "q" to exit' 
     while True:
         # Get a fresh frame
-        (depth, _), (rgb, _) = get_depth(), get_video()
+        val, bgr = stream.read()
         
         # Build a two panel color image
-        d3 = np.dstack((depth, depth, depth)).astype(np.uint8, copy=False)
-        bgr = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
-        da = np.hstack((d3, bgr)).astype(np.uint8, copy=False)
+#         bgr = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
         
 #         src = cv2.cv.fromarray(da)
-        cv2.imshow('both', da)
-        video.write(da)
-        k = cv2.waitKey(5)
+        cv2.imshow('stream', bgr)
+        video.write(bgr)
+        k = cv2.waitKey(1)
         if (k > -1) and (k < 256):
             if chr(k)=='q':
 #                 video.release()
