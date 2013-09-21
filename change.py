e32db2c5e3b35af216eb9bfbc4d0848c5d7d54d9
ramykl <r.loveridge@hotmail.com>
2013-09-21 18:16:27 +1000

    changed

--- change.py
+++ change.py
@@ -11,6 +11,8 @@ message = 'waiting for button press'
 lowerThresh = 0
 upperThresh = 255
 scale = 1  # 8 for 11 bit data
+save = False # flag for saving video
+rotate = True # flag for rotated video
 
 lowThreshold = 50
 max_lowThreshold = 100
@@ -19,13 +21,15 @@ kernel_size = 3
 h = 0
 w = 0
 c = 0
+sec = 270
 
 cropHeight = 0 #0.55 # 0.55 for kinect images
 
 path = "./videos/"
-file = 5
-video2 = cv2.VideoWriter(path+'convert'+str(file)+'.avi',cv2.cv.CV_FOURCC('D','I','V','X'),20,(1280,480))
-circ = np.zeros((480,640,3),np.uint8)
+if save:
+    file = 5
+    video2 = cv2.VideoWriter(path+'convert'+str(file)+'.avi',cv2.cv.CV_FOURCC('D','I','V','X'),20,(1280,480))
+    circ = np.zeros((480,640,3),np.uint8)
 
 #     k = cv2.waitKey(5)
 #     if (k > -1) and (k < 256):
@@ -42,64 +46,55 @@ def video(stream):
             break
         if crop:
             frame = frame[0:h,w/2:w]
+        if rotate:
+            frame = cv2.flip(frame, -1)
         process(frame)
         val, frame = stream.read()
-
-# # Used for Canny edge detection       
-# def CannyThreshold(lowThreshold, gray, img):
-#     detected_edges = cv2.GaussianBlur(gray,(5,5),0)
-#     detected_edges = cv2.Canny(detected_edges,lowThreshold,lowThreshold*ratio,apertureSize = kernel_size)
-#     dst = cv2.bitwise_and(img,img,mask = detected_edges)  # just add some colours to edges from original image.
-#     
-#     if debug:
-#         print message
-#         cv2.imshow('canny demo',dst)
-#         cv2.waitKey()
-#     
-
     
 def process(image):
     hc, wc, cc = np.shape(image)
     cropIm = image[hc*cropHeight:hc,0:wc]
-    if debug:
-        cv2.imshow('original', image)
-        cv2.waitKey(1)
+#     if debug:
+#         cv2.imshow('original', image)
+# #         cv2.waitKey(1)
         
     grayIm = cv2.cvtColor(cropIm, cv2.COLOR_BGR2GRAY)
+#     if debug:    
+#         cv2.imshow('Gray',grayIm)
+#         cv2.waitKey(10)
     
-#     """
-#     Canny edge detection
-#     """
-#     cv2.namedWindow('canny demo')
-#     
-# #     def thresh(lowThreshold):
-# #         CannyThreshold(cv2.getTrackbarPos('Min threshold', 'canny demo'), grayIm, cropIm) # initialization
-# # 
-# #     cv2.createTrackbar('Min threshold','canny demo',lowThreshold, max_lowThreshold, thresh)
-# #     
-# 
-#     CannyThreshold(lowThreshold, grayIm, cropIm) # initialization
-#     
-
+    
     """
     Houghcircles detection
     """
     img = cv2.medianBlur(grayIm,3)
+#     img = cv2.medianBlur(thresh,5)
+#     r, img = cv2.threshold(img, 83, 0, cv2.THRESH_TOZERO)
+#     r, img = cv2.threshold(img, 200, 0, cv2.THRESH_TOZERO_INV)
+    r, img = cv2.threshold(img, 85, 0, cv2.THRESH_TOZERO_INV)
+    if debug:    
+        cv2.imshow('thresh',img)
+        cv2.waitKey(1)
     cimg = cropIm
-    circles = cv2.HoughCircles(img,cv2.cv.CV_HOUGH_GRADIENT,1,200,param1=100,param2=32,minRadius=35,maxRadius=90)
+    circles = cv2.HoughCircles(img,cv2.cv.CV_HOUGH_GRADIENT,1,400,param1=100,param2=45,minRadius=50,maxRadius=200)
     if circles is not None:
-        if len(circles[0]) < 3:
+        if len(circles[0]) < 2:
+            count = 0
             for i in circles[0,:]:
-                cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),1)  # draw the outer circle
-                cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)     # draw the center of the circle
-            if debug:
-                
-                cv2.imshow('detected circles',cimg)
+                if (i[1] < (sec+80)) and (i[1] > (sec-80)):
+                    count += 1
+                    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),1)  # draw the outer circle
+                    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)     # draw the center of the circle
+            if count:
+                if debug:    
+                    cv2.imshow('detected circles',cimg)
 #                 print message
-                cv2.waitKey(10)
-                circ[hc*cropHeight:hc,0:wc] = cimg
-    da = np.hstack((image, circ)).astype(np.uint8, copy=False)
-    video2.write(da)
+                    cv2.waitKey(10)
+                    if save:
+                        circ[hc*cropHeight:hc,0:wc] = cimg
+    if save:
+        da = np.hstack((image, circ)).astype(np.uint8, copy=False)
+        video2.write(da)
     
 
 def main(inputfile):
--- saveVideo.py
+++ saveVideo.py
@@ -7,13 +7,14 @@ import subprocess
 # x = 1280
 # y = 720
 
-
 def doloop():
     path = "./videos/"
-    num = glob.glob(str(path) + "*.avi")
+    num = glob.glob(str(path) + "video*.avi")
     i = len(num)
     
-    
+    """
+    to get video number for specific camera look in /dev/v4l/by-id "ls -l" 
+    """
     p1 = subprocess.Popen(['ls', '-l', '/dev/v4l/by-id'], stdout=subprocess.PIPE) #, '|', 'grep', 'HD-3000']
     p2 = subprocess.Popen(['grep', 'HD-3000'], stdin = p1.stdout, stdout=subprocess.PIPE)
     p1.stdout.close()
@@ -24,22 +25,28 @@ def doloop():
     cam = int(out[-2])
     
     """
-    to get video number for specific camera look in /dev/v4l/by-id "ls -l" 
+    create video capture stream
     """
     stream = cv2.VideoCapture(cam)    
     if not stream.isOpened():
         print 'error opening stream'
         sys.exit(1)
         
+    """
+    create video writing stream
+    """
     video = cv2.VideoWriter(path+'video'+str(i)+'.avi',cv2.cv.CV_FOURCC('D','I','V','X'),20,(640,480))
     if not video.isOpened():
         print 'error with video opening'
         sys.exit(1)
     
-    subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Exposure, Auto', "3"])
+    # 1 for manual, 3 for auto
+    subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Exposure, Auto', "1"])
+    # Exposure level 5, 10, 20, 39, 78, 156, 312, ... [roughly double value] (darkest to lightest)
+    subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Exposure (Absolute)', "10"]) 
     subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Contrast', "1"])
     subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Brightness', "150"])
-    subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Saturation', "75"])
+    subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Saturation', "50"])
     subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Sharpness', "25"])
     
 #     stream.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, y)
@@ -59,7 +66,6 @@ def doloop():
         k = cv2.waitKey(1)
         if (k > -1) and (k < 256):
             if chr(k)=='q':
-#                 video.release()
                 cv2.destroyAllWindows()
                 sys.exit(0)
         
