bc6e5062da41cfcf1efc9440a26de7a240845d60
ramykl <r.loveridge@hotmail.com>
2013-10-24 17:53:18 +1000

    commented

--- videoStream.py
+++ videoStream.py
@@ -8,6 +8,7 @@ import Queue
 import select
 import time
 
+# camera name to be found
 camera = "C920"
 # camera = 'HD-3000'
 
@@ -18,9 +19,8 @@ def doloop():
     i = len(num)
     
     print camera
-    """
-    to get video number for specific camera look in /dev/v4l/by-id "ls -l" 
-    """
+    
+    # gets camera video number by searching in /dev/v4l/by-id with the camera name 
     p1 = subprocess.Popen(['ls', '-l', '/dev/v4l/by-id'], stdout=subprocess.PIPE) #, '|', 'grep', 'HD-3000']
     p2 = subprocess.Popen(['grep', camera], stdin = p1.stdout, stdout=subprocess.PIPE)
     p1.stdout.close()
@@ -28,8 +28,10 @@ def doloop():
     if out == '':
         print 'webcam not attached'
         sys.exit(1)
-    cam = int(out[-2])
     
+    # camera number
+    cam = int(out[-2])
+    # settings for the logitech C920 webcam
     if camera == 'c920':
         subprocess.Popen(['v4l2-ctl', '-d', '/dev/video'+str(cam), '--set-fmt-video=width=640,height=480,pixelformat=1'])
         subprocess.Popen(['v4l2-ctl', '-d', '/dev/video'+str(cam), '--set-parm=30'])
@@ -52,7 +54,8 @@ def doloop():
         subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Exposure, Auto Priority', "1"])
         subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Focus, Auto', "0"])
         subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Focus (Absolute)', "0"])
-        
+    
+    # settings for the Microsoft HD-3000 webcam 
     elif camera == 'HD-3000':
         # 1 for manual, 3 for auto
         subprocess.Popen(['uvcdynctrl', '-dvideo'+str(cam), "-s",  'Exposure, Auto', "1"])
@@ -83,41 +86,49 @@ def doloop():
         sys.exit(1)
     
     time.sleep(1)
+    # create two queues for passing data between threads
     flag = Queue.Queue(0)
     pic = Queue.Queue(0)
+    
+    # create and start thread to capture frames 
     t1 = threading.Thread(target=get_frames, args=(flag, pic, stream, video))
     t1.setDaemon(True)
     t1.start()
     print "press 'q' to exit, press 's' to toggle show images"
-    k = 0 
+    k = 0
     while True:
+        #check for image from capture thread
         try:
             im = pic.get_nowait()
         except Queue.Empty:
             im = None
-
+        
+        # display image if available
         if im is not None:
             cv2.imshow('stream', im)
             cv2.waitKey(1)
             im = None
         
-        
+        # check for input from terminal
         i,o,e = select.select([sys.stdin],[],[],0.01)
         if (i):
             k = sys.stdin.readline().strip()
-            
+        
+        # check input for specific commands
         if k == 'q':
+            # quit program
             flag.put_nowait(k)
             t1.join()
             cv2.destroyAllWindows()
             sys.exit(0)
         if k == 's':
+            # toggle display frames
             flag.put_nowait(k)
             time.sleep(1)
             cv2.destroyAllWindows()
             k = 0
 
-     
+# thread for getting frames from webcam and saving them
 def get_frames(flag, pic, stream, video):
     show = False
     count = 0
@@ -132,19 +143,25 @@ def get_frames(flag, pic, stream, video):
         except Queue.Empty:
             temp = 0
         
+        # toggle show
         if temp == 's':
             show = not show
             print "show stream:", show
             count = 0
             temp = 0
             if not show:
+                # clear queue if show is turned off
                 pic.queue.clear()
         if show:
             count += 1
+            # show only every 5th frame to limit processing
             if count %5 == 0:
                 pic.put_nowait(bgr)
+        # quit program
         if temp == 'q':
+            # end thread
             print "exiting"
             break
-        
+
+# run main function
 doloop()
